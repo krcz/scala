673e4686c20aa5b77bed9acf6d797d6252df485d
Marcin Kurczych <krcz@krcz.eu>
2019-03-09 00:54:52 +0100

    Solve acronym

--- exercises/acronym/src/main/scala/Acronym.scala
+++ exercises/acronym/src/main/scala/Acronym.scala
@@ -1,3 +1,12 @@
 object Acronym {
-  def abbreviate(phrase: String): String = ???
+  def abbreviate(phrase: String): String = {
+    phrase
+      .toLowerCase
+      .replace("-", " ")
+      .filter(c => c.isLetter || c == ' ')
+      .split(" ")
+      .filter(_.nonEmpty)
+      .map(part => part.head.toUpper)
+      .mkString("")
+  }
 }
