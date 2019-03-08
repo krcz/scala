b83cbdb9b3df55074cacd68510c8bfaf5dd5e145
Marcin Kurczych <krcz@krcz.eu>
2019-03-09 00:46:16 +0100

    Solve accumulate

--- exercises/accumulate/src/main/scala/Accumulate.scala
+++ exercises/accumulate/src/main/scala/Accumulate.scala
@@ -1,3 +1,12 @@
 class Accumulate {
-  def accumulate[A, B](f: (A) => B, list : List[A]): List[B] = ???
+  def accumulate[A, B](f: (A) => B, list : List[A]): List[B] = {
+    def _accumulate(rest: List[A], acc: List[B]): List[B] = {
+      rest match {
+        case Nil => acc.reverse
+        case head :: tail =>
+          _accumulate(tail, f(head) :: acc)
+      }
+    }
+    _accumulate(list, Nil)
+  }
 }
