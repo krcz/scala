a3705b66e908161f914caa7d23c5c8a96cf52069
Marcin Kurczych <krcz@krcz.eu>
2019-03-09 19:28:15 +0100

    Solve zipper

--- exercises/zipper/src/main/scala/Zipper.scala
+++ exercises/zipper/src/main/scala/Zipper.scala
@@ -1,33 +1,62 @@
+import scala.annotation.tailrec
+
+case class Zipper[A] private (node: BinTree[A], parentZipper: Option[(Zipper.Direction, Zipper[A])])
+
 object Zipper {
-  // A zipper for a binary tree.
-  // ??? Zipper[A] ???
+  sealed trait Direction
+  case object Left extends Direction
+  case object Right extends Direction
 
   // Get a zipper focussed on the root node.
-  def fromTree[A](bt: BinTree[A]): Zipper[A] = ???
+  def fromTree[A](bt: BinTree[A]): Zipper[A] = {
+    Zipper(bt, None)
+  }
 
   // Get the complete tree from a zipper.
-  def toTree[A](zipper: Zipper[A]): BinTree[A] = ???
+  def toTree[A](zipper: Zipper[A]): BinTree[A] = {
+    up(zipper) match {
+      case Some(z) => toTree(z)
+      case None => zipper.node
+    }
+  }
 
   // Get the value of the focus node.
-  def value[A](zipper: Zipper[A]): A = ???
+  def value[A](zipper: Zipper[A]): A = {
+    zipper.node.value
+  }
 
   // Get the left child of the focus node, if any.
-  def left[A](zipper: Zipper[A]): Option[Zipper[A]] = ???
+  def left[A](zipper: Zipper[A]): Option[Zipper[A]] = {
+    zipper.node.left.map(leftNode => Zipper(leftNode, Some((Left, zipper))))
+  }
 
   // Get the right child of the focus node, if any.
-  def right[A](zipper: Zipper[A]): Option[Zipper[A]] = ???
+  def right[A](zipper: Zipper[A]): Option[Zipper[A]] = {
+    zipper.node.right.map(rightNode => Zipper(rightNode, Some((Right, zipper))))
+  }
 
   // Get the parent of the focus node, if any.
-  def up[A](zipper: Zipper[A]): Option[Zipper[A]] = ???
+  def up[A](zipper: Zipper[A]): Option[Zipper[A]] = {
+    zipper.parentZipper.map {
+      case (Left, pzip) => pzip.copy(node = pzip.node.copy(left = Some(zipper.node)))
+      case (Right, pzip) => pzip.copy(node = pzip.node.copy(right = Some(zipper.node)))
+    }
+  }
 
   // Set the value of the focus node.
-  def setValue[A](v: A, zipper: Zipper[A]): Zipper[A] = ???
+  def setValue[A](v: A, zipper: Zipper[A]): Zipper[A] = {
+    zipper.copy(node = zipper.node.copy(value = v))
+  }
 
   // Replace a left child tree.
-  def setLeft[A](l: Option[BinTree[A]], zipper: Zipper[A]): Zipper[A] = ???
+  def setLeft[A](l: Option[BinTree[A]], zipper: Zipper[A]): Zipper[A] = {
+    zipper.copy(node = zipper.node.copy(left = l))
+  }
 
   // Replace a right child tree.
-  def setRight[A](r: Option[BinTree[A]], zipper: Zipper[A]): Zipper[A] = ???
+  def setRight[A](r: Option[BinTree[A]], zipper: Zipper[A]): Zipper[A] = {
+    zipper.copy(node = zipper.node.copy(right = r))
+  }
 }
 
 // A binary tree.
