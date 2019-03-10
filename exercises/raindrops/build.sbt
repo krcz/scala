d6ef768dffcc3c690f33895d9e37b9a91e5d180d
Marcin Kurczych <krcz@krcz.eu>
2019-03-11 00:13:54 +0100

    Solve raindrops

--- exercises/raindrops/build.sbt
+++ exercises/raindrops/build.sbt
@@ -1,3 +1,5 @@
 scalaVersion := "2.12.1"
 
 libraryDependencies += "org.scalatest" %% "scalatest" % "3.0.1" % "test"
+
+excludeFilter := HiddenFileFilter || "example*"
--- exercises/raindrops/src/main/scala/Raindrops.scala
+++ exercises/raindrops/src/main/scala/Raindrops.scala
@@ -1,4 +1,10 @@
 object Raindrops {
-  def convert(n: Int): String = ???
+
+  val sounds = List(3 -> "Pling", 5 -> "Plang", 7 -> "Plong")
+
+  def convert(n: Int): String = {
+    val sound = sounds.flatMap { case (k, v) => if (n % k == 0) Some(v) else None }.mkString("")
+    if (sound.nonEmpty) sound else n.toString
+  }
 }
 
